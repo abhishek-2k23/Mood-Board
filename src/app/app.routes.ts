f789dc0e6e43cf41b0bde755616bacec67129ccb
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-04-14 23:01:00 +0530

    component creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '/',
+        component: HeaderComponent,
+    },
+    {
+        path : 'about',
+        component: AboutComponent,
+    }
+];
