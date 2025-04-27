ee6478537b80a299d2db1c6f805a462511b6a5b5
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-04-27 08:55:54 +0530

    add header, footer and routing for home and about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './layout/home/home.component';
+import { AboutComponent } from './layout/about/about.component';
 
 export const routes: Routes = [
     {
         path: '',
         loadComponent: () => HomeComponent
+    },
+    {
+        path: 'about',
+        loadComponent : () => AboutComponent
     }
 ];
