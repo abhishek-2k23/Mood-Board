0af68bd39d88df5b21804ce1f00ee31f2687f481
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-04-27 09:18:07 +0530

    add moods button and text based on mood selection

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,12 +1,24 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  username = "Abhishek"
+  moods = [
+    'happy', 'sad', 'calm', 'excited', 'angry'
+  ]
+  isMoodSelected=false
+  selectedMood = ''
+
+  selectMood(mood:string):void {
+    this.isMoodSelected = true;
+    this.selectedMood = mood;
+  }
 
 }
