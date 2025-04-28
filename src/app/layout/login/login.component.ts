4875c46051bc0346977d8fc5a48850c4a2672967
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-04-28 20:54:36 +0530

    modified to banana in a box property

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../service/user/user.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   constructor(private router : Router, private userservice: UserService){}
   userName = ''
-  setUserName(event: Event){
-    this.userName = (event.target as HTMLInputElement).value;
-  }
+  // setUserName(event: Event){
+  //   this.userName = (event.target as HTMLInputElement).value;
+  // }
   login(){
     if(this.userName.trim() !== ''){
       this.userservice.loginUser(this.userName);
